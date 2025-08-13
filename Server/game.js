0c434450734db649aa7687d127f1df5e2e8d3a22
mason Passinault <passinaultm@gmail.com>
2025-08-13 18:05:41 -0400

    multiple game instance support

--- Server/game.js
+++ Server/game.js
@@ -1,176 +1,184 @@
-import Player from "../shared/player.js"
-import Barrier from "../shared/barrier.js"
-import Collisions from "../shared/collisions.js"
+import Player from "../shared/player.js";
+import Barrier from "../shared/barrier.js";
+import Collisions from "../shared/collisions.js";
 
-export default function Game(io) {
-    this.io = io
-    this.startDelay = 3 // seconds
-    this.gameLength = 60// seconds
-    this.playerWidth = 50
-    this.playerHeight = 100
-    this.mapWidth = 800
-    this.mapHeight = 600 
+export default function Game(io, playerIds) {
+    this.io = io;
+    this.startDelay = 3; // seconds
+    this.gameLength = 60; // seconds
+    this.playerWidth = 50;
+    this.playerHeight = 100;
+    this.mapWidth = 800;
+    this.mapHeight = 600;
 
     this.players = {};
-    this.bullets = {}
+    this.bullets = {};
     this.bulletIdCounter = 0;
-    this.gameRunning = false
-    this.gameInterval = null
-    this.startTimeout = null
-    this.endTimeout = null
-    this.connections = 0
+    this.gameRunning = false;
+    this.gameInterval = null;
+    this.startTimeout = null;
+    this.endTimeout = null;
+
+    this.playerIds = playerIds;
 
     this.startTimer = function() {
-        let timer = this.gameLength
-        this.io.emit('startTimer', this.startDelay)
+        let timer = this.gameLength;
+
+        // Notify players the game will start soon
+        this.playerIds.forEach(id => {
+            const socket = this.io.sockets.sockets.get(id);
+            if (socket) socket.emit('startTimer', this.startDelay);
+        });
 
         setTimeout(() => {
-            this.startGame()
-            let endGameTimer = setInterval(() => {
-                this.io.emit('timerUpdate', timer)
-                timer--
-            }, 1000)
+            this.startGame();
+
+            const endGameTimer = setInterval(() => {
+                this.playerIds.forEach(id => {
+                    const socket = this.io.sockets.sockets.get(id);
+                    if (socket) socket.emit('timerUpdate', timer);
+                });
+                timer--;
+            }, 1000);
 
             this.startTimeout = setTimeout(() => {
-                clearInterval(endGameTimer)
-                this.endGame('Game ended due to time')
-            }, (this.gameLength + 1) * 1000) 
+                clearInterval(endGameTimer);
+                this.endGame('Game ended due to time');
+            }, (this.gameLength + 1) * 1000);
 
-        }, (this.startDelay + 1) * 1000)
-    }
+        }, (this.startDelay + 1) * 1000);
+    };
 
     this.updateGameState = () => {
-        this.io.emit('gameState', this.gameRunning)
-    }
-
+        this.playerIds.forEach(id => {
+            const socket = this.io.sockets.sockets.get(id);
+            if (socket) socket.emit('gameState', this.gameRunning);
+        });
+    };
 
     this.startGame = function () {
+        const collisions = new Collisions(this.mapWidth, this.mapHeight);
 
-        // creating the collision manager, storing game height and width
-        const collisions = new Collisions(this.mapWidth, this.mapHeight)
+        const barrier = new Barrier(this.mapWidth / 2 - 25, this.mapHeight / 2 - 100, 50, 200);
+        const platform = new Barrier(0, this.mapHeight / 2 + this.playerHeight / 2, 50, 5);
+        const platform2 = new Barrier(this.mapWidth - platform.width, platform.y, 50, 5);
 
-        // adding game objects to the collision manager
-        const barrier = new Barrier(this.mapWidth / 2 - 25, this.mapHeight / 2 - 100, 50, 200,)
-        const platform = new Barrier(0, this.mapHeight/2 + this.playerHeight/2, 50, 5 )
-        const platform2 = new Barrier(this.mapWidth - platform.width, platform.y, 50, 5)
+        collisions.addGameObject(barrier);
+        collisions.addGameObject(platform);
+        collisions.addGameObject(platform2);
 
-        collisions.addGameObject(barrier)
-        collisions.addGameObject(platform)
-        collisions.addGameObject(platform2)
+        const [p1Id, p2Id] = this.playerIds;
+        this.players[p1Id] = new Player(p1Id, 10, this.mapHeight / 2 - this.playerHeight / 2 - 3, true);
+        this.players[p2Id] = new Player(p2Id, this.mapWidth - this.playerWidth - 10, this.mapHeight / 2 - this.playerHeight / 2 - 3, false);
 
-        // creating the players
-        let pList = Object.keys(this.players)
+        this.gameRunning = true;
 
-        this.players[pList[0]] = new Player(pList[0], 10, this.mapHeight/2 - this.playerHeight/2 - 3, true)
-
-        this.players[pList[1]] = new Player(pList[1], this.mapWidth - this.playerWidth - 10 ,this.mapHeight/2 - this.playerHeight/2 - 3, false)
-
-        this.gameRunning = true
-        this.io.emit('currentPlayers', this.players)
-        this.io.emit('startGame')
+        // Notify only the players in this game
+        this.playerIds.forEach(id => {
+            const socket = this.io.sockets.sockets.get(id);
+            if (socket) {
+                socket.emit('currentPlayers', this.players);
+                socket.emit('startGame');
+            }
+        });
 
         this.gameInterval = setInterval(() => {
-
-            // checking for the win condtion of the game
-            for (const id in this.players) {
-                const player = this.players[id]
-                if (player.p1 && player.x >= this.mapWidth) {
-                    this.endGame('Player one is the winner!')
-                } else if (!player.p1 && player.x < 0) { 
-                    this.endGame('Player two is the winner!')
-                }
-            }
-
-            // this first for loop handles updating the players for collisions
+            // Check win condition
             for (const id in this.players) {
                 const player = this.players[id];
+                if (player.p1 && player.x >= this.mapWidth) this.endGame('Player one is the winner!');
+                if (!player.p1 && player.x < 0) this.endGame('Player two is the winner!');
+            }
 
+            // Update player positions
+            for (const id in this.players) {
+                const player = this.players[id];
                 if (player.state === 'moving') {
-
-                    // if out of bounds player snaps to border
                     if (collisions.outOfBounds(player)) {
                         if (player.x <= 0) player.x = 1;
                         if (player.x + this.playerWidth >= this.mapWidth) player.x = this.mapWidth - this.playerWidth - 1;
                         if (player.y <= 0) player.y = 1;
                         if (player.y + this.playerHeight >= this.mapHeight) player.y = this.mapHeight - this.playerHeight - 1;
-                        collisions.stopPlayer(player)
-
-                        // if colides with object player moves back and stops outside of object
-                    } else if (collisions.playerCollision(player)){
-                        player.x -= player.dx * 5
-                        player.y -= player.dy * 5
-                        collisions.stopPlayer(player)
-
-                        // player moves if there is no collision
+                        collisions.stopPlayer(player);
+                    } else if (collisions.playerCollision(player)) {
+                        player.x -= player.dx * 5;
+                        player.y -= player.dy * 5;
+                        collisions.stopPlayer(player);
                     } else {
-                        player.x += player.dx * 5
-                        player.y += player.dy * 5
+                        player.x += player.dx * 5;
+                        player.y += player.dy * 5;
                     }
                 }
 
-                this.io.emit('playerMoved', player); 
+                // Emit to only players in this game
+                this.playerIds.forEach(pid => {
+                    const socket = this.io.sockets.sockets.get(pid);
+                    if (socket) socket.emit('playerMoved', player);
+                });
             }
 
-            // bullet logic
+            // Update bullets
             for (const id in this.bullets) {
-                const bullet = this.bullets[id]
+                const bullet = this.bullets[id];
 
-                const outOfBounds = collisions.outOfBounds(bullet) 
-                const collidesWithBarrier = collisions.playerCollision(bullet)
+                const outOfBounds = collisions.outOfBounds(bullet);
+                const collidesWithBarrier = collisions.playerCollision(bullet);
 
-                // checking if bullet collides with every player
-                for (const id in this.players) {
-                    const player = this.players[id]
-
+                for (const pid in this.players) {
+                    const player = this.players[pid];
                     if (collisions.bulletCollidesWithPlayer(bullet, player)) {
-                        const [centerX, centerY] = player.center
-
-                        const dirX = centerX - bullet.x
-                        const dirY = centerY - bullet.y
-
-                        const magnitude = Math.sqrt(dirX * dirX + dirY * dirY)
-                        player.dx = dirX / magnitude 
-                        player.dy = dirY / magnitude 
-                        bullet.ttl = 0
+                        const [centerX, centerY] = player.center;
+                        const dirX = centerX - bullet.x;
+                        const dirY = centerY - bullet.y;
+                        const magnitude = Math.sqrt(dirX * dirX + dirY * dirY);
+                        player.dx = dirX / magnitude;
+                        player.dy = dirY / magnitude;
+                        bullet.ttl = 0;
                     }
                 }
 
-                // checking if bullet hits walls or objects
                 if (collidesWithBarrier || outOfBounds) {
                     if (collisions.bulletCollidesHorizontal(bullet, barrier)) {
-                        bullet.dy = -bullet.dy
+                        bullet.dy = -bullet.dy;
                     } else {
-                        bullet.dx = -bullet.dx
+                        bullet.dx = -bullet.dx;
                     }
-                    bullet.ttl--
-                } 
+                    bullet.ttl--;
+                }
 
-                // if the bullet has no lifes left delete, otherwise move
-                if (bullet.ttl < 1 ) {
-                    delete this.bullets[id]
-                    this.io.emit('deleteBullet', id)
-
+                if (bullet.ttl < 1) {
+                    delete this.bullets[id];
+                    this.playerIds.forEach(pid => {
+                        const socket = this.io.sockets.sockets.get(pid);
+                        if (socket) socket.emit('deleteBullet', id);
+                    });
                 } else {
-                    bullet.x += bullet.dx * 9
-                    bullet.y += bullet.dy * 9
-                    this.io.emit('bulletMove', {id, x: bullet.x, y: bullet.y })
+                    bullet.x += bullet.dx * 9;
+                    bullet.y += bullet.dy * 9;
+                    this.playerIds.forEach(pid => {
+                        const socket = this.io.sockets.sockets.get(pid);
+                        if (socket) socket.emit('bulletMove', { id, x: bullet.x, y: bullet.y });
+                    });
                 }
             }
         }, 33);
-    }
+    };
 
     this.endGame = function(text) {
-        this.gameRunning = false
-        clearTimeout(this.startTimeout)
-        clearTimeout(this.endTimeout)
-        clearInterval(this.gameInterval)
+        this.gameRunning = false;
+        clearTimeout(this.startTimeout);
+        clearTimeout(this.endTimeout);
+        clearInterval(this.gameInterval);
 
         for (const id in this.players) {
-            this.players[id] = null
+            this.players[id] = null;
         }
 
-        this.io.emit('endGame', text)
-        this.updateGameState()
-    }
-}
+        this.playerIds.forEach(id => {
+            const socket = this.io.sockets.sockets.get(id);
+            if (socket) socket.emit('endGame', text);
+        });
 
+        this.updateGameState();
+    };
+}
--- Server/server.js
+++ Server/server.js
@@ -1,71 +1,115 @@
-import express from 'express'
-import http from 'http'
-import { Server } from 'socket.io'
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
-import Bullet from "../shared/bullet.js"
-import Game from "./game.js"
+import Bullet from "../shared/bullet.js";
+import Game from "./game.js";
 
 const app = express();
 app.use(express.static('../Client/public'));
 const server = http.createServer(app);
 const io = new Server(server);
 
-const game = new Game(io)
+// Queue for matchmaking
+const queue = [];
+
+// Active games map
+const games = {}; // key: gameId, value: Game instance
+let gameIdCounter = 1;
 
 io.on('connection', (socket) => {
-    game.players[socket.id] = null
+    console.log(`Player connected: ${socket.id}`);
+    queue.push(socket.id);
 
+    // Check if we can start a new game (2 players per game)
+    if (queue.length >= 2) {
+        const player1Id = queue.shift();
+        const player2Id = queue.shift();
+        const newGameId = gameIdCounter++;
+        const game = new Game(io, [player1Id, player2Id]);
+        games[newGameId] = game;
 
-    if (!game.gameRunning && io.engine.clientsCount == 2) {
-      game.startTimer()
+        // Assign game reference to sockets
+        io.sockets.sockets.get(player1Id).gameId = newGameId;
+        io.sockets.sockets.get(player2Id).gameId = newGameId;
+
+        game.startGame();
+        console.log(`Game ${newGameId} started with players: ${player1Id}, ${player2Id}`);
     }
 
+    // Player movement
     socket.on('setMove', (data) => {
-        let player = game.players[socket.id]
+        const game = games[socket.gameId];
+        if (!game) return;
+
+        const player = game.players[socket.id];
+        if (!player) return;
+
+        player.dx = data.dx;
+        player.dy = data.dy;
+        player.state = 'moving';
+
+        io.to(socket.id).emit('updateMoving', {
+            id: socket.id,
+            dx: player.dx,
+            dy: player.dy,
+            state: 'moving'
+        });
+    });
+
+    // Mouse movement
+    socket.on('mouseMove', (data) => {
+        const game = games[socket.gameId];
+        if (!game) return;
+        const player = game.players[socket.id];
         if (player) {
-            player = game.players[socket.id]
-            player.dx = data.dx
-            player.dy = data.dy
-            player.state = 'moving'
-            io.emit('updateMoving',{
-                id: socket.id,
-                dx:player.dx,
-                dy:player.dy,
-                state:'moving'
-            })
+            player.mouseX = data.mouseX;
+            player.mouseY = data.mouseY;
         }
     });
 
+    // Shooting
+    socket.on('shoot', (data) => {
+        const game = games[socket.gameId];
+        if (!game) return;
+
+        const bulletId = game.bulletIdCounter++;
+        game.bullets[bulletId] = new Bullet(bulletId, data.x, data.y, data.dx, data.dy, socket.id);
+
+        io.emit('newBullet', game.bullets[bulletId]);
+    });
+
+    // Disconnect
     socket.on('disconnect', () => {
-        if (game.players[socket.id]) {
-            delete game.players[socket.id];
-            game.endGame()
+        console.log(`Player disconnected: ${socket.id}`);
+
+        // Remove from queue if still waiting
+        const queueIndex = queue.indexOf(socket.id);
+        if (queueIndex !== -1) {
+            queue.splice(queueIndex, 1);
         }
+
+        // Remove from game if in one
+        const game = games[socket.gameId];
+        if (game) {
+            delete game.players[socket.id];
+            game.endGame(); // Optional: could also handle partially remaining players
+
+            // Remove game if empty
+            if (Object.keys(game.players).length === 0) {
+                delete games[socket.gameId];
+            }
+        }
+
         io.emit('playerDisconnected', socket.id);
     });
-
-    socket.on('mouseMove', (data) => {
-        let player = game.players[socket.id]
-        player.mouseX = data.mouseX
-        player.mouseY = data.mouseY
-    })
-
-    socket.on('shoot', (data) => {
-        const bulletId = game.bulletIdCounter++
-
-        game.bullets[bulletId] = new Bullet(bulletId, data.x, data.y, data.dx, data.dy, socket.id)
-        io.emit('newBullet', game.bullets[bulletId])
-    })
-})
-
+});
 
 server.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
-    console.log();
     console.log('---- Endr Server Stats ----');
 
-    // Log the connected client count every second
     setInterval(() => {
-        process.stdout.write(`Connections: ${io.engine.clientsCount}\r`)
-    },500)
+        process.stdout.write(`Connections: ${io.engine.clientsCount}\r`);
+    }, 500);
 });
